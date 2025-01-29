a992e8d617ed65243106f111ca4453ac3574e4db
Sebastjan Bas <60714956+generalseba@users.noreply.github.com>
2025-01-28 23:09:17 -0800

    set up the project (paths, tailwind configuration)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // './src/**/*.{html,js}',
+    // './src/renderer/src/**/*.{js,ts,jsx,tsx,mdx}',
+    // './src/renderer/src/components/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/renderer/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
